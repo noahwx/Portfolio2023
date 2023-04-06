8286fc2a4d95b4f757517dace954ad5962ae6edb
noahwx <noahfontenette@outlook.com>
2023-04-05 22:24:03 -0500

    0.7.060

--- port-app/src/components/ContactForm.js
+++ port-app/src/components/ContactForm.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+
+const FORM_ENDPOINT = "https://public.herotofu.com/v1/eaada180-cf59-11ed-bffe-d5e6f853e30c";
 
 const ContactForm = () => {
+
+    const [submitted, setSubmitted] = useState(false);
+    const handleSubmit = () => {
+        setTimeout(() => {
+        setSubmitted(true);
+        }, 100);
+    };
+
+    if (submitted) {
+        return (
+        <>
+            <h2 className="submit-text">Thank you!</h2>
+            <div className="submit-text">We'll be in touch soon.</div>
+        </>
+        );
+    }
+
     return ( 
-        <form>
+        <form
+            action={FORM_ENDPOINT}
+            onSubmit={handleSubmit}
+            method="POST"
+            target="_blank"
+        >
             <label For="name">Name</label>
             <input type="text" id="name" name="name" placeholder="Your name.."></input>
             <label For="email">Email</label>
