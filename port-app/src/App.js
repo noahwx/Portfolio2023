97b4068714abde08483497ae1f08411dd74292e9
noahwx <noahfontenette@outlook.com>
2023-04-06 20:57:56 -0500

    0.7.705

--- port-app/src/App.js
+++ port-app/src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import Favicon from 'react-favicon';
-import screenfull from 'screenfull';
 import logo from './logo.svg';
 import './index.css';
 import Header from './components/Header';
 import Main from './components/Main';
 
 // VK      M.I.SSS
-// Version 0.7.702
+// Version 0.7.705
 
 function App() {
-
-  if (screenfull.isEnabled) {
-    screenfull.request();
-  }
 
   return (
     <>
