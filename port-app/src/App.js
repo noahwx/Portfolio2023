7a7bc3bbae8db3a5606345bcfb666cba2b962669
noahwx <noahfontenette@outlook.com>
2023-04-06 20:49:33 -0500

    0.7.700

--- port-app/src/App.js
+++ port-app/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import Favicon from 'react-favicon';
+import screenfull from 'screenfull';
 import logo from './logo.svg';
 import './index.css';
 import Header from './components/Header';
 import Main from './components/Main';
 
 // VK      M.I.SSS
-// Version 0.7.631
+// Version 0.7.700
 
 function App() {
+
+  const element = document.getElementById('target');
+
+  document.getElementById('button').addEventListener('click', () => {
+    if (screenfull.isEnabled) {
+      screenfull.request(element, {navigationUI: 'hide'});
+    }
+  });
+
   return (
     <>
       <Favicon url={logo} />
